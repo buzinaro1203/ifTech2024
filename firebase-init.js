823bb14711d6f6499710c843eb942436ad0de5f5
Buzinaro1203 <guilhermebuzinaro212@gmail.com>
2024-10-10 10:27:12 -0300

    First Commit :fire:

---
+++ firebase-init.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTNhFE-13RSjXkqXkySnt4gNqoOPXmph8",
+  authDomain: "phmeter-83d7e.firebaseapp.com",
+  projectId: "phmeter-83d7e",
+  storageBucket: "phmeter-83d7e.appspot.com",
+  messagingSenderId: "13425144594",
+  appId: "1:13425144594:web:f71a3f8e26f49c9f9939cb",
+  measurementId: "G-XF90R3TCNL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
---
+++ pages/register/register.js
@@ -0,0 +1,98 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    window.location.href = "pages/home/home.html";
+  }
+})
+function onChangeEmail() {
+  const email = document.getElementById("email").value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
+  form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+  toggleRegisterButtonDisable()
+
+}
+function onChangePassword() {
+  const password = document.getElementById("password").value;
+  form.passwordRequiredError().style.display = password ? "none" : "block";
+
+  form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
+  toggleRegisterButtonDisable()
+}
+function onChangeConfirmPassword() {
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const password = document.getElementById("password").value;
+
+  form.confirmPasswordDoesntMatchError().style.display = password == confirmPassword ? "none" : "block";
+  toggleRegisterButtonDisable()
+}
+function toggleRegisterButtonDisable() {
+  form.registerButton().disabled = !isFormValid();
+}
+
+function isFormValid() {
+  const email = form.email().value;
+  if (!email || !validateEmail(email)) {
+    return false;
+  }
+
+  const password = form.password().value;
+  if (!password || password.length < 6) {
+    return false;
+  }
+
+  const confirmPassword = form.confirmPassword().value;
+  if (password != confirmPassword) {
+    return false;
+  }
+
+  return true;
+}
+
+
+function register() {
+  showLoading()
+  const email = form.email().value;
+  const password = form.password().value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+
+    .then(() => {
+      // Signed in
+      window.location.href = "../home/home.html"
+      hideLoading()
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      hideLoading()
+      alert(getErrorMessage(error))
+
+    });
+}
+
+
+
+
+
+function getErrorMessage(error) {
+  return error.message
+
+}
+
+
+
+const form = {
+  email: () => document.getElementById('email'),
+  password: () => document.getElementById('password'),
+  confirmPassword: () => document.getElementById('confirmPassword'),
+  emailInvalidError: () => document.getElementById('email-invalid-error'),
+  emailRequiredError: () => document.getElementById('email-required-error'),
+  passwordMinLengthError: () => document.getElementById('password-min-length-error'),
+  passwordRequiredError: () => document.getElementById('password-required-error'),
+  confirmPasswordDoesntMatchError: () => document.getElementById('password-doesnt-match-error'),
+  registerButton: () => document.getElementById('registerButton'),
+
+
+
+
+
+
+}

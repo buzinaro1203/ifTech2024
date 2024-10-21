61b13a1117ea73a329949fdc38683c1748cb2c75
Buzinaro1203 <guilhermebuzinaro212@gmail.com>
2024-10-20 22:42:17 -0300

    i still trying to read de data

--- tsReadData.js
+++ tsReadData.js
@@ -1,3 +1,15 @@
-import data from 'https://thingspeak.mathworks.com/channels/2682894/fields/1.json?results=1';
-console.log(data);
-
+function fetchJSONData() {
+  fetch("https://thingspeak.mathworks.com/channels/2682894/fields/1.json?results=1")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error
+          (`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) =>
+      console.log(data))
+    .catch((error) =>
+      console.error("Unable to fetch data:", error));
+}
+fetchJSONData();

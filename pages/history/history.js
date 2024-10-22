77533bf07f3c84ae16923a556b884bf07fbbf772
Buzinaro1203 <guilhermebuzinaro212@gmail.com>
2024-10-22 01:33:55 -0300

    quase

---
+++ pages/history/history.js
@@ -0,0 +1,57 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("Usuário autenticado:", user);
+    findMedicoes(user);
+  } else {
+    console.log("Nenhum usuário autenticado.");
+  }
+});
+
+function findMedicoes(user) {
+  showLoading();
+
+  firebase.firestore().collection('medicoes').where('user', '==', user.uid).orderBy('date', 'desc').get().then((querySnapshot) => {
+    hideLoading();
+    querySnapshot.forEach((doc) => {
+      const medicoes = doc.data();
+      addMedicoesToScreen([medicoes]);
+
+
+    })
+
+  });
+
+
+}
+
+
+function addMedicoesToScreen(medicoes) {
+  const historyTable = document.getElementById('historyTable');
+  medicoes.forEach(medicoes => {
+  });
+  for (let i = 0; i < medicoes.length; i++) {
+    const row = document.createElement('tr');
+    historyTable.appendChild(row);
+
+
+
+    const date = document.createElement('td');
+    const timestamp = medicoes[i].data;
+    const dateObj = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
+    date.innerHTML = dateObj.toLocaleString();
+    row.appendChild(date);
+
+
+    const phValue = document.createElement('td');
+    phValue.innerHTML = medicoes[i].phValue;
+    row.appendChild(phValue);
+
+    const tsID = document.createElement('td');
+    tsID.innerHTML = medicoes[i].tsID;
+    row.appendChild(tsID);
+
+
+  }
+}
+
+
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,45 @@
-console.log('aaaa')
+
+var datatest;
+var phValue;
+var timestamp;
+var id;
+
+window.registrarMedicao = () => {
+
+  fetch("https://thingspeak.mathworks.com/channels/2682894/fields/1.json?results=1")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error
+          (`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      data = JSON.parse(JSON.stringify(data.feeds));
+
+      phValue = data[0].field1;
+      timestamp = data[0].created_at;
+      id = data[0].entry_id;
+
+      const medicao = {
+        "phValue": phValue,
+        "timestamp": timestamp,
+        "tsID": id,
+        "user": {
+          "uid": firebase.auth().currentUser.uid
+        }
+
+      }
+      console.log(medicao);
+
+    })
+    .catch((error) =>
+      console.error("Unable to fetch data:", error));
+
+
+}
+
+
 function logout() {
   showLoading();
   firebase.auth().signOut().then(() => {
